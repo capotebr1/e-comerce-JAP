41b6278e7c0e06ec0c3eb1d5520493fb70b2eaad
CapoteBr1 <brunetecapote@gmail.com>
2022-08-14 19:22:55 -0300

    email and password redirect

--- js/login.js
+++ js/login.js
@@ -1,10 +1,10 @@
-const email = document.getElementById("floatingInput");
-const password = document.getElementById("floatingPassword");
 
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("formulario").addEventListener('submit', () => {
-        if(email.value > 0 && password.value > 8){
-            window.location.href = "/index.html";
+document.getElementById("submit-button").addEventListener("click", () =>{
+        let email = document.getElementById("floatingInput").value;
+        let password = document.getElementById("floatingPassword").value;
+        let emailRegex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
+        if(password.length >= 8 && email.match(emailRegex)){
+                alert("funca");
+                window.location.href = "index.html";
         }
-    }); 
-  });
+}); 
