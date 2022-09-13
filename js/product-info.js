d037204749ef161bcb496f732b8b8a9d0c848888
capotebr1 <brunetecapote@gmail.com>
2022-09-12 22:56:53 -0300

    se agrega cambio

--- js/product-info.js
+++ js/product-info.js
@@ -13,7 +13,7 @@ const descripcion = document.getElementById("descripcion");
 const puntaje = document.getElementById("score");
 const infoProducto = document.querySelector("#main-description");
 let comentarios;
-fetch(Url) //cambio
+fetch(Url) //cambioa
 .then(response => response.json())
 .then(data => {
     console.log(data);
